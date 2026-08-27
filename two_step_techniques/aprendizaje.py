a3d4458b87ec4451fdc632f9796a3d54271b4d32
trisgt <tristanpg2004@gmail.com>
2026-08-27 17:54:18 +0200

    Fix for seeds in CNN

--- two_step_techniques/aprendizaje.py
+++ two_step_techniques/aprendizaje.py
@@ -109,6 +109,10 @@ def mlp(X, y, indices_RN, semilla = None, balanceo_clases = False):
 
 # Red Neuronal Convolucional (CNN):
 def cnn(X, y, indices_RN, semilla = None, balanceo_clases = False):
+    # Si se ha dado una semilla, la utilizamos con TensorFlow:
+    if semilla is not None:
+        tf.random_set_seed(semilla)
+
     # Preparamos los datos. En este caso, no se aplanan:
     X, y = preparado_datos(X, y, aplanar = False)
 
