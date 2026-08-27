e476e18ee5e5ee4cdbf24578efe3c42d9c6cc7d2
trisgt <tristanpg2004@gmail.com>
2026-08-27 17:13:10 +0200

    Small name changes for consistency

--- evaluacion/evaluacion.py
+++ evaluacion/evaluacion.py
@@ -12,7 +12,7 @@ def informacion_evaluacion(
         path_guardado = "modelo.txt"):
     
     # Información para el usuario:
-    print(f"\nEvaluacion de \"{nombre}\"")
+    print(f"\nEvaluacion: {nombre}")
     print(f"\tClases Positivas (PU): {clases_positivas}")
     print(f"\tPorcentaje de Positivos (PU): {porc_positivos}")
     print(f"\tMetrica Utilizada (PU): {metrica}")
@@ -23,7 +23,7 @@ def informacion_evaluacion(
     if guardar:
         with open(path_guardado, "a") as f:
             # Información para el usuario:
-            f.write(f"Evaluacion de \"{nombre}\"")
+            f.write(f"Evaluacion: {nombre}")
             f.write(f"\n\tClases Positivas (PU): {clases_positivas}")
             f.write(f"\n\tPorcentaje de Positivos (PU): {porc_positivos}")
             f.write(f"\n\tMetrica Utilizada (PU): {metrica}")
@@ -34,12 +34,12 @@ def informacion_evaluacion(
 
 
 # Función para evaluar los resultados de un modelo tras entrenamiento, con F1-Score y PR-AUC:
-def evaluacion_dataset(y_true, y_pred, y_score):
+def evaluacion_dataset(y_test, y_pred, y_score):
     # Para PR-AUC, calculamos los valores de precision y recall a partir de la curva:
-    vals_precision, vals_recall, _ = precision_recall_curve(y_true, y_score)
+    vals_precision, vals_recall, _ = precision_recall_curve(y_test, y_score)
 
     metricas = {
-        "F1-Score": f1_score (y_true, y_pred, zero_division = 0),
+        "F1-Score": f1_score(y_test, y_pred, zero_division = 0),
         "PR-AUC": auc(vals_recall, vals_precision)
     }
 
