ced6fc6c6830fe7777f96929f120e97449467635
trisgt <tristanpg2004@gmail.com>
2026-08-28 17:48:28 +0200

    Correction

--- config/preproc_config.py
+++ config/preproc_config.py
@@ -3,7 +3,7 @@ NUM_FOLDS = 5
 SEMILLA_SPLIT = 42
 
 # Parámetros comunes para la conversión del dataset a PU:
-PORCENTAJE_POS = 0.6    # Porcentaje de positivos visibles tras la conversión a PU
+PORCENTAJE_POS = 0.5    # Porcentaje de positivos visibles tras la conversión a PU
 SEMILLA_PR = 42
 
 # Parámetros comunes para los embeddings en datasets de texto:
