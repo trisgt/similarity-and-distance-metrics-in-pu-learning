0ce99c8ae2cf44cd19cba8c0b3426426bb92ad0d
trisgt <tristanpg2004@gmail.com>
2026-08-28 17:36:34 +0200

    Correction

--- config/train_config.py
+++ config/train_config.py
@@ -1,11 +1,11 @@
 # Selección de métodos para la obtención de Negativos Fiables y de Aprendizaje:
-MET_NEG_FIABLES = "KNN"
-MET_APRENDIZAJE = "Logistic Regression"
+MET_NEG_FIABLES = "Rocchio"
+MET_APRENDIZAJE = "Random Forest"
 
 # Parámetros comunes para la obtención de Negativos Fiables (dentro de los Two-Step Methods):
 METRICA = "euclidean"
 K = 10                  # En KNN, K-Means, K-Medoids y CRNE
-PORCENTAJE_RN = 0.2     # En KNN, K-Means y K-Medoids
+PORCENTAJE_RN = 0.5     # En KNN, K-Means y K-Medoids
 SEMILLA_RN = 42         # En K-Means, K-Medoids y CRNE
 
 # Parámetros comunes para el Aprendizaje:
